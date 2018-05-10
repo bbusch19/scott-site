ecf1c872e7172e7347287483913d61339cc949c6
Breiden Busch <breiden.busch@gmail.com>
2018-05-10 10:21:38 -0600

    Added sass support

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,5 @@ module.exports = {
   siteMetadata: {
     title: 'Scott Sucks Realty',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-sass'],
 }
